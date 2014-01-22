72c26cee459983aeeac8498eb78ceeaf2c2dfcd1
Alexsandro Almeida <dvp.alex@yahoo.com>
2014-01-22 11:24:02 -0200

    Start

---
+++ ayp/Ninb.Apps.Email/Entity/Acount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ninb.Apps.Email.Entity
+{
+    public class Acount
+    {
+        public String Name
+        {
+            get;
+            set;
+        }
+
+        public 
+
+
+    }
+}
